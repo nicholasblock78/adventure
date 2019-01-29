7880e8751e738c2162ecc6b0643bb7980c12fa44
Nick Block <nicholasblock78@gmail.com>
2019-01-29 12:24:24 -0600

    Add in Users and Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get 'sessions/new'
+  post 'sessions/create'
+  delete 'sessions/destroy'
+  post 'users/update' => 'users#update'
+
+  resources 'users' do 
+  end
+
   root 'site#index'
 end
