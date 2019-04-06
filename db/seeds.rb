d2131a87b644a95d36f13c53834b8349245ed41e
Nick Block <nicholasblock78@gmail.com>
2019-04-06 08:57:48 -0500

    add in travel db seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -79,3 +79,30 @@ Post.create({
     preview: "In summer 2018, I visited Boston for a weekend in June for a TD Ameritrade investor's educational seminar. Outside of the my time there, I was able to amply explore and experience Boston.",
     body: ""
 })
+
+Post.create({
+    title: "Amsterdam",
+    main_img: "https://res.cloudinary.com/blockscloud/image/upload/v1554558963/DSC_0060h_rdmj4x.jpg",
+    category_id: 4,
+    user_id: 1,
+    preview: "Over 2019 Spring Break, I visited Europe for 8 days. I spent my first two in Amsterdam.",
+    body: ""
+})
+
+Post.create({
+    title: "Zermatt",
+    main_img: "https://res.cloudinary.com/blockscloud/image/upload/v1554558646/IMG_1010_fii3xb.jpg",
+    category_id: 4,
+    user_id: 1,
+    preview: "Over 2019 Spring Break, I visited Europe for 8 days. I spent a day skiing in picturresque Zermatt",
+    body: ""
+})
+
+Post.create({
+    title: "Barcelona",
+    main_img: "https://res.cloudinary.com/blockscloud/image/upload/v1554558917/DSC_0989h_typigt.jpg",
+    category_id: 4,
+    user_id: 1,
+    preview: "Over 2019 Spring Break, I visited Europe for 8 days. I ended my trip with the last two days in Barcelona.",
+    body: ""
+})
