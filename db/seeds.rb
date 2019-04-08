8b6aab830c7f01f9c84d6d2e44211b1703de3424
Nick Block <nicholasblock78@gmail.com>
2019-04-07 22:39:08 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,6 +62,15 @@ Post.create({
 })
 
 Post.create({
+    title: "Personal Taxes Broken Down",
+    main_img: "",
+    category_id: 3,
+    user_id: 1,
+    preview: "Living in Chicago, it's sad to see over a quarter of your paycheck vanish everytime you're paid. ",
+    body: ""
+})
+
+Post.create({
     title: "Punta Cana",
     main_img: "https://res.cloudinary.com/blockscloud/image/upload/v1548829431/IMG_0364_toxddt.jpg",
     category_id: 4,
@@ -105,4 +114,30 @@ Post.create({
     user_id: 1,
     preview: "Over 2019 Spring Break, I visited Europe for 8 days. I ended my trip with the last two days in Barcelona.",
     body: ""
+})
+
+Post.create({
+    title: "5 Common Mistakes Most Home Chefs Make That You Can Fix Right Now",
+    main_img: "https://i.ytimg.com/vi/AFwkGTEles8/maxresdefault.jpg",
+    category_id: 2,
+    user_id: 1,
+    preview: "5 Common Mistakes Most Home Chefs Make That You Can Fix Right Now",
+    body: "
+    Here are the top five most common mistakes people make in their kitchens
+    Using too much heat
+    Have you ever wondered why you burned the outside of your food but barely cooked the inside? Or ever uncontrollably/accidentally generated a lot of smoke in the kitchen? The culprit -- too much heat. The temptation is too crank the heat first starting out to get the pan hot as fast as possible. I know I don’t want to sit there any longer than I have to when waiting for a pan to heat up. However, in doing so, you’re almost guaranteeing your food won’t cook properly. From this point forward, consider HIGH heat off limits. You should only ever use this in specific cases when you know why and how you’re doing it (searing meat for example). The vast majority of food will want to be cooked on MEDIUM/MEDIUM-HIGH heat. As a general rule of thumb, the longer something needs to cook for, the lower the heat needs to be. Heat and use your pans on MEDIUM heat. As you master other cooking skills, you can start experimenting with the heat.
+    2. Your knife is too dull
+            This mistake is hard to understand until you’ve ever used a quality chef’s knife. There’s an easy way to tell if your knife is up to snuff-- the paper test. The paper test is simple; to administer, take a regular piece of paper and try to cut it with your knife in the care (**please exercise common sense and caution while performing the paper test!**). If the knife “takes” the edge of the paper and cuts through it, your knife is sharp. If the knife just knocks the paper out of the way or does anything other than cut the paper, you need to sharpen or upgrade your knife. This is important and a dull knife is actually a safety hazard. A high-quality chef’s knife should glide through almost all foods--hardly any “cutting” is needed. However, when the knife is dull, you are forced to cut and apply pressure yourself. The knife is no longer sharp enough to do the work for you. As you apply force to make up for the knife’s dull edge, the odds of your hand slipping increase greatly. 
+            There’s a cheap and easy to sharpen and hone your knife. Use the bottom of a ceramic plate or bowl. You can also pick up a knife sharpener at most grocery stores, retail stores etc.. 
+    Knife too dull
+    Learn the basic commercial tricks that you need to work in any kitchen
+    https://www.amazon.com/Victorinox-Fibrox-Chefs-Knife-8-Inch/dp/B008M5U1C2/
+    
+    3. Putting the food in too early
+    
+    4. Playing with the food too much
+    
+    5. 
+    
+    "
 })
