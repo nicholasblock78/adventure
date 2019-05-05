5ac80de16efcb24613beff1b936aaa22aa8d3627
Nick Block <nicholasblock78@gmail.com>
2019-05-05 15:42:19 -0500

    some basic post styling

---
+++ db/seeds/build_comp.rb
@@ -0,0 +1,89 @@
+
+Post.create({
+    title: "Building my Computer",
+    sub_title: "",
+    main_img: "",
+    category_id: 3,
+    user_id: 1,
+    preview: "",
+    body: "<h3>MY STORY</h3>
+
+    The need for a more powerful computer arose while attempting to edit my graduate class group’s final video project, a 20 minute endeavor my laptop, a 2017 15” MacBook Pro, found a bit challenging. My machine started to really ramp up to where both the fan and heat became concerns. Unfortunately for me, I found myself hooked in this creative process. I’d only ever had direct cause to do video editing during a summer I spent in Atlanta, and even then, most of the editing duties fell to another classmate. This time, however, I was the sole person capable, defaulting me into full responsibility. Not only did I want to create video, I also wanted to create animations and start to learn After Effects, another Adobe video-intensive program. In addition to the hands on experience, I was looking to prove myself technically and creatively. Buying a computer would enable my creative outbursts but any desktop I decided powerful enough was easily $1,000. Building a computer would allow me to prove myself technically and then allow my creative pursuits, all at half the cost. 
+    
+    After a few introductory Google searches, I decided that building a computer was well within my reach. I remember being a kid and hearing about a couple other kids at school building their own computers (kids that have since gone on to Ivy leagues and are successful engineers). Back then, building a computer was difficult. Parts were more finicky and if anything went sideways, which it often did, you were all on your own. There was no Internet or Geek Squad to come save you. All you had was your trusty Command Line and your own intuition. However, nowadays, parts had more cross-compatibility making the build process easier than ever. So, with my decision made, I set my budget at $500 and pressed onward.
+    
+    <h3>THE INVESTIGATION PROCESS</h3>
+    
+    I started by looking at computer cases. This actually proved to be a time consuming process. The options are endless. I 
+    
+    The first step of buying the important internal parts started by looking at CPUs, also known as the computer’s processor. After all, this usually is the first thing marketed about a computer, is its power and speed. I started looking at Intel. Intel has long been held as the best CPU maker out there. All of the machines my family had when I was growing up were Intel-based machines. They sound cool, too. Their line is i3, i5, i7...like the BMWs. So, an i7 quad core processor seems pretty neat. Unfortunately, the newest released lines are expensive. Luckily for me, AMD had really started to step up its game. AMD’s Ryzen processors rivaled Intel’s chipsets power at a much cheaper price point. After reading numerous reviews and articles about CPUs, I decided on the AMD Ryzen 5 2600. Amazon had it for 35% off! For $165, I got six cores of 3.4 GHz power. 
+    
+    I didn’t quite pull the trigger on purchasing the CPU just quite yet. The next step was to pick a motherboard, because as I’d come to discover, a CPU and motherboard (affectionately termed as “mobo” in the online communities) needed to be compatible. Every mobo had a CPU socket that needed to match the CPU you wished to install. In my case, I wanted AMD’s AM4 socket. As the options available and possible combinations are mind numbing, I used user reviews as the main differentiator for my buying decisions. Unfortunately, I didn’t find a single motherboard under $200 with more than four stars. The mobo pricing got out of hand quickly. Mega gaming motherboards that look out of a sci-fi movie easily ran up over a grand. From what I could tell, these expensive boards were all for intense gaming and “overclocking” - the sport of pushing your computer’s processing power to the limit for the sake of bragging rights. Other than that, the more expensive boards offered more excess connectors and support for crazy powerful GPUs (graphical processing units). I eventually settled on the ASRock B450M Pro board, that seemed to be the most reliable according to the reviews. It offered a great mix of modern future-proof features and most importantly, it matched the micro ATX format my case needed.
+    
+    Probably the least exciting purchase of all was the PSU, also known as the power supply unit. For this, I could care less, I just wanted it to work. Since I wasn’t going to be doing any of the overclocking or be plugging in a lot of extras, I knew I could get away with less research here as my need for power wasn’t as great. Thankfully, Amazon had an overwhelming favorite a near five star review. I quickly decided on the __ and moved on. 
+    
+    One of the future-proof slots in my mobo, the M2.SATA slot, enabled me to buy a specialized SSD that could be used as my boot disk. Essentially, installing my software on this would make my computer quick and snappy. The last component I needed to worry about for the speed and power of my build was the computer’s memory, or RAM. A few more Google searches revealed that another important piece to my build -- the CPU and RAM needed to be near the same speed/power to maximize their use. A good analogy is … Since I was on a budget I settled, on lower overall RAM but optimizing that lesser RAM’s speed. This is one of the first things I plan on upgrading once I have the funds. 
+    
+     I couldn’t find any major price differences on Amazon for the SSD or memory sticks so I decided to buy both in person at Best Buy given a few gift certificates I had left over. You can also see both on Amazon here and here. 
+    
+    The last thing I figured I should get since I wanted to be using computer-intensive Adobe programs was a basic graphics card. I had no need for the crazy complex double GPUs that the intense gamers use. I wasn’t going to be editing in 4K either. I had plenty of other equipment that would need to be upgraded to the 4K level first. I could definitely wait on a high end graphics card for now. Fifty bucks got me a great deal on this entry-level NVIDIA card. 
+    
+    My apartment building has a very poor package receiving area. Living in Chicago, I’m lucky enough to have one of Amazon’s shipping stores. I had everything delivered there except for the computer case, and the SSD and memory sticks I picked up at Best Buy. 
+    
+    
+    
+        <h3>THE BUILD PROCESS</h3>
+    With all the various parts laid out on my dining table, I started to feel a level of intimidation and fear set in. I’d now made the $500 investment, but I was on my own. It’d take another hundred or two to correct my failure if I needed to resort to Geek Squad’s help. Would I actually be able to do this? Have I just thrown away this money because I tried to cut some corners? (Me being the naive consumer that isn’t technical enough for such a techie endeavor.)
+    
+    I decided to wade in slowly. I started with some Youtube videos and ‘How to’ articles then let those sink in. I repeated this process a couple times, varying the material consumer each time to help ensure I left no rock overturned. Eventually, I decided I was ready. 
+    
+    A big caveat I should mention before I go any further -- every single article I came across referencing building or even simply upgrading a machine, had a big warning stapled across its front, be wary of static electricity. Just as how rubbing a balloon against your head make your hair stand up, rubbing my hands on my newly purchased parts could silently render them useless. Many suggested I go buy an “Anti Static Bracelet” but being on a budget and not wanting to make another trip to Best Buy or wait any longer, I forged ahead. I certainly didn’t feel staticy and winter’s grip over my apartment had finally faded. (During the winter months, I could product static electricity on command.) My fix to this problem -- flip flops. 
+    
+    First thing’s first, installing the power supply. The bulkiest and most cable heavy piece you’re putting into your machine is nice to get out of the way up front. Routing these cables ahead of time is much easier before you screw in your motherboard. Installing the PSU, was super easy. There’s was only one place it could go and only one way it could go into the slot. It slid right in and I tightened four screws to hold into place. Done. That simple.
+    
+    Next, was to configure some of the motherboard installation before we actually placed the board into the case. The first part was installing the CPU onto the motherboard. I’m finally handling the expensive parts of my build and this is where the real worry of static electricity came to play. I could feel the stakes rise with each next step. 
+    
+        <h3>MOMENT OF TRUTH</h3>
+    Finally, I think everything is ready. I close up the case and turn off the TV I had on in the background. I want to ensure I can hear everything. Who knows? Maybe I’m about to blow something out. Should I have the fire extinguisher close by? Is this $500 about to go snap-fizzle-crackle-pop and up in smoke? I’d long considered buying one of those little Raspberry Pi computers to mess around and expose myself to this deeper technical learnings, and here I was - skipping straight to a full blown computer. The stakes were slightly higher than I’d maybe hoped. Rest assured; pushing the power button for the first time was surely an exhilarating moment. 
+    
+    Once I pushed the power button, I could hear the sound of a fan start to whir. I sat for what seemed like an eternity waiting for my screen to show flickers of hope. Anything to show me that I’d taken a step in the right direction and hadn’t broken the parts I’d only just received. 
+    
+    No such luck. Not a few seconds into having pressed ‘Power’, the computer started to beep. A sigh of relief, quickly followed by a giant wave of anxiety set in. Beeping meant that I had done at least something right. I’d brought all of these parts together to get beeping. Looking back on it, I’m not sure what I would have done if I had not received any beeping. The initial excitement of beeping was quickly overridden by the fear of, “now what?”. I’d realized there had been some blind hope that putting this computer together would simply work for me. Dream big. 
+    
+    I shut the computer down and opened it back up. I tried turning the computer back on, this time determined to ensure I counted the number of beeps, because as it turns out, these beeps aren’t random, but a representation of basic SOS-type language for communicating basic error messages when there’s no display available. 
+    
+    Naively, I thought it may simply work the second time on booting up. Maybe the first time was a fluke! To no avail. I was actually so hopeful that it’d magically work the second time that I forgot to count the beeps, however, this time I noticed that the CPU fan wasn’t spinning. Voila! Something was wrong that I’d definitely need to fix. A next step presented itself. With this knowledge, I bypassed counting the beeps and moved to get the CPU fan working. Maybe that was the cause of the beeping. 
+    
+    To prepare for some modifications, I unplugged everything again and slipped back into my flip flops. I was able to locate the CPU power cable relatively easy, but the cord wouldn’t reach across the case. I had to uninstall the PSU and reroute this one cable so that it’d reach. With that installed correctly, I plugged everything back in and readied for Round 2. 
+    
+    Round 2 brought back the beeps but hey, at least my CPU fan was now spinning. I’d call that a success. Now, I finally remembered to count the beeps. Five. A few google searches later and I came to the conclusion that a graphics card was needed. Confusing fine print on the ASRock’s website made me think I could get away with a graphics card and plug my HDMI directly into. With hindsight’s aid, I recognized that my CPU needed graphics capabilities to empower the motherboard’s graphics inputs. Essentially, I had a couple dead ports on my board and needed a graphics card in order to display anything. I had picked up this MSI NVIDIA card last minute due to its high positive reviews and cheap price on Amazon. It looked to be an efficient solution to my dual monitor problem and something I should have after all if I was going to be doing video editing. Thank goodness I did because it allowed me to solve this problem immediately. I repeated the process of unplugging everything and opening up the case. This time I was installing the graphics card. A straightforward process, yet slightly tricky to execute since this involved two points of connection - the motherboard and external facing inputs. This fit to the outside of the case needed to be snug to prevent excess dust from sneaking into my case. (Dust is can be both harmful and gross if left unchecked.) With some cautious fiddling, I got the graphics card installed and readied for try #3. This time I didn’t bother closing the case back up. I figured I was in for a long night of a slow trial and error process to get this computer to work. Yet, voila! I pressed the power button and sure enough, I had a display! Now, I wasn’t quite home free yet. The Windows 10 USB boot drive media I’d created wasn’t functioning properly. But, everything was running properly. No more beeping. Simply the low rumbling sound of some whirring fans and my display full of error messages. It was beautiful. I triumphantly closed the machine for a final time indefinitely.
+    
+    UPDATE: I finally installed the WD Blue 1TB Internal HDD into my computer. The main delay was I needed to get to Best Buy to buy a HDD bracket and SATA cord. Evidently, the hard drive I bought off Amazon was included neither. 
+    
+    <h3>FINAL PRODUCT</h3>
+    The real two options available for the operating system are Windows and Ubuntu. Windows is a paid license whilst the latter is open-source, making it free. After my first attempt at the Windows 10 boot drive failed, I gave Ubuntu try. 
+    
+    **I am unsure how I would have bypassed this without the use of Parallels VM machine software. This enabled me to easily program an EXFAT formatted drive, an integral part to install the software elsewhere.
+    
+    I opted for Windows, specifically Windows 10. I’d been exposed enough to it over the years where I was comfortable with what I was getting myself into. 
+    
+    The final build
+            Thermaltake Mid Tower Micro ATX Tower Case (Versa H17)
+        Power Supply Unit (EVGA 500W)
+        AMD Ryzen 5 2600 Processor
+            ASRock MicroATX Motherboard (B450M PRO)
+        Graphics Card (MSI Gaming GeForce GT 710 2 GB GDRR3)
+            Desktop Memory (Corsair Vengeance LPX 16gb 3.0GHz DDR4 [2x8gb])
+        Samsung NVMe SSD card (280gb)
+        Hard drive (WD Blue 1tb ~~ yet to be installed)
+    
+    <h3>FINAL VERDICT</h3>
+    The process from start (researching and ideation of what I was creating) to finish (fully functioning desktop computer) was truly an enjoyable one. Ultimately, I started this process to help save me money from buying an expensive pre-built machine from Apple or Dell. I’d strayed from my budget once I’d purchased the majority of the parts, so to gain a clear picture of the money I’d saved, I ventured back to Best Buy’s website to see where my new computer stacked up. To my surprise, this answer became impossible to answer. My machine didn’t stack up. I’d built a machine tailored to my own specific needs. Not some brand’s general presumption of what consumers want. My machine didn’t fit in and that was a good thing. A moment of real pride swept over me. I’d built something unique, something that I could truly call my own. Even though it was technological in nature, I knew it’d stand the test of time (at least for the next decade or so). I can easily upgrade it at any time. I can easily troubleshoot the hardware myself. Who needs a Genius Bar anyways?
+    
+    I also learned a few more deeper tidbits of Computer Science that you’d only pick up on if you were majoring in the field in school.
+    
+    My computer is also snappy. It boots from shut down consistently in under 10 seconds (this includes me entering my basic password). 
+    
+    
+    "
+})
--- db/seeds/seed_posts.rb
+++ db/seeds/seed_posts.rb
@@ -77,22 +77,28 @@ Post.create({
     category_id: 2,
     user_id: 1,
     preview: "5 Common Mistakes Most Home Chefs Make That You Can Fix Right Now",
-    body: "
-    Here are the top five most common mistakes people make in their kitchens
-    Using too much heat
+    body: "5 Common Mistakes Most Home Chefs Make That You Can Fix Right Now
+
+    Here are the top five most common mistakes people make in their kitchens:
+    <h3>1. Using too much heat</h3>
     Have you ever wondered why you burned the outside of your food but barely cooked the inside? Or ever uncontrollably/accidentally generated a lot of smoke in the kitchen? The culprit -- too much heat. The temptation is too crank the heat first starting out to get the pan hot as fast as possible. I know I don’t want to sit there any longer than I have to when waiting for a pan to heat up. However, in doing so, you’re almost guaranteeing your food won’t cook properly. From this point forward, consider HIGH heat off limits. You should only ever use this in specific cases when you know why and how you’re doing it (searing meat for example). The vast majority of food will want to be cooked on MEDIUM/MEDIUM-HIGH heat. As a general rule of thumb, the longer something needs to cook for, the lower the heat needs to be. Heat and use your pans on MEDIUM heat. As you master other cooking skills, you can start experimenting with the heat.
-    <h2>2. Your knife is too dull</h2>
-            This mistake is hard to understand until you’ve ever used a quality chef’s knife. There’s an easy way to tell if your knife is up to snuff-- the paper test. The paper test is simple; to administer, take a regular piece of paper and try to cut it with your knife in the care (**please exercise common sense and caution while performing the paper test!**). If the knife “takes” the edge of the paper and cuts through it, your knife is sharp. If the knife just knocks the paper out of the way or does anything other than cut the paper, you need to sharpen or upgrade your knife. This is important and a dull knife is actually a safety hazard. A high-quality chef’s knife should glide through almost all foods--hardly any “cutting” is needed. However, when the knife is dull, you are forced to cut and apply pressure yourself. The knife is no longer sharp enough to do the work for you. As you apply force to make up for the knife’s dull edge, the odds of your hand slipping increase greatly. 
-            There’s a cheap and easy to sharpen and hone your knife. Use the bottom of a ceramic plate or bowl. You can also pick up a knife sharpener at most grocery stores, retail stores etc.. 
+    <h3>2. Your knife is too dull</h3>
+        This mistake is hard to understand until you’ve ever used a quality chef’s knife. There’s an easy way to tell if your knife is up to snuff-- the paper test. The paper test is simple; to administer, take a regular piece of paper and try to cut it with your knife in the care (**please exercise common sense and caution while performing the paper test!**). If the knife “takes” the edge of the paper and cuts through it, your knife is sharp. If the knife just knocks the paper out of the way or does anything other than cut the paper, you need to sharpen or upgrade your knife. This is important and a dull knife is actually a safety hazard. A high-quality chef’s knife should glide through almost all foods--hardly any “cutting” is needed. However, when the knife is dull, you are forced to cut and apply pressure yourself. The knife is no longer sharp enough to do the work for you. As you apply force to make up for the knife’s dull edge, the odds of your hand slipping increase greatly. 
+        There’s a cheap and easy to sharpen and hone your knife. Use the bottom of a ceramic plate or bowl. You can also pick up a knife sharpener at most grocery stores, retail stores etc.. 
     Knife too dull
-    Learn the basic commercial tricks that you need to work in any kitchen
-    https://www.amazon.com/Victorinox-Fibrox-Chefs-Knife-8-Inch/dp/B008M5U1C2/
+    Learn the basic commercial tricks that you need to work in any kitchen. Buy your <a href='https://www.amazon.com/Victorinox-Fibrox-Chefs-Knife-8-Inch/dp/B008M5U1C2/ref=as_li_ss_tl?keywords=chef+knife&qid=1557087655&s=gateway&sr=8-4&linkCode=ll1&tag=20sadventures-20&linkId=a1eae05903e668424929543ab12c4e3e&language=en_US' target='_blank'>very own chef's knife</a>!
     
-    <h1>3. Putting the food in too early</h1>
+    <h3>3. Putting the food in too early</h3>
+        Many people throw their food and pan on the stove altogether at once. This is a big no no. By doing this, you drastically change the cooking process. You always want to add food to an already heated pan. This allows for the delicious flavorings you find in searing and caramelization.
     
-    4. Playing with the food too much
+        <h3>4. Playing with the food too much</h3>
+        Have you ever heard the expression, “a watched pot never boils”? This stems from the fact that if you keep uncovering a pot to check if the water is boiling, it’ll never actually boil because you keep releasing the very heat required to start that boil. The same thing goes with sauteing food -- if you keep moving food around in a pan, you don’t let any browning of the food to occur, and this is where the real flavor is. Patience is required in cooking; and if you’ve followed the advice from Step #1 and reduced your heat, you can afford to be a little more patient. 
     
-    5. 
+    <h3>5. Not enough salt when boiling water</h3>
+        I’m sure all of you have heard that salt, or sodium, is bad for your heart. The problem, however, isn’t salt. It’s processed food, a conglomeration of fat, sugar, and salt. Yet, salt’s inclusion on this trio had led to all of the current fad diets promoting the restriction of salt. This had leaked into home cooking as well. Many cooks are afraid of adding too much salt, when there’s no need to be. However, there is a difference between boiling water for pasta and boiling water for rice. Do you know the difference? Hopefully, you answered the water. With pasta, you drain the water, thus losing all of the salt. The pasta will have absorbed a minority of the salt you added to the water. Conversely, rice will absorb all of the salt you add because the rice absorbs all of the water in its cooking process.
+        Mixing up the pots of boiling waters is only a mistake you’ll make once. Rice made from salty pasta water is virtually inedible. 
+        **Pro Tip** Since rice absorbs all of the water it’s cooked in, it becomes very easy to season your rice during the cooking process. Add whatever spices you want. Substitute vegetable broth for water. Add a few dashes of a flavored oil. 
+    
     
     "
 })
